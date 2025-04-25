f2215c954779ca613fd1bd11a915d19fc2b52471
beyzakececi <beyzakececi.35@gmail.com>
2025-04-26 01:20:48 +0300

    feat: add Authors and Books controllers with CRUD operations

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,6 +1,74 @@
-﻿namespace LibraryManagementSystem.Controllers;
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class AuthorsController
+namespace LibraryManagementSystem.Controllers
 {
-    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public AuthorsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+                return NotFound();
+
+            return author;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Author>> CreateAuthor(Author author)
+        {
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAuthor(int id, Author author)
+        {
+            if (id != author.Id)
+                return BadRequest();
+
+            _context.Entry(author).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var author = await _context.Authors.Include(a => a.Books).FirstOrDefaultAsync(a => a.Id == id);
+
+            if (author == null)
+                return NotFound();
+
+            if (author.Books.Any())
+                return BadRequest("Cannot delete an author who has books.");
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
 }
--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,6 +1,71 @@
-﻿namespace LibraryManagementSystem.Controllers;
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class BooksController
+namespace LibraryManagementSystem.Controllers
 {
-    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+                return NotFound();
+
+            return book;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book book)
+        {
+            if (id != book.Id)
+                return BadRequest();
+
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+                return NotFound();
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
 }
